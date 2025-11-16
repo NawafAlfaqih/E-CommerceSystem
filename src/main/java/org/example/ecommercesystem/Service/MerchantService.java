a20d136918fca7c984a966bc4075fad81dea4ff5
Nawaf Alfaqih <n4waf.xhd@gmail.com>
2025-11-16 15:57:01 +0300

    Adding User Class

--- src/main/java/org/example/ecommercesystem/Service/MerchantService.java
+++ src/main/java/org/example/ecommercesystem/Service/MerchantService.java
@@ -37,5 +37,4 @@ public class MerchantService {
         }
         return false;
     }
-
 }
---
+++ src/main/java/org/example/ecommercesystem/Service/UserService.java
@@ -0,0 +1,41 @@
+package org.example.ecommercesystem.Service;
+
+import org.example.ecommercesystem.Model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService {
+
+    ArrayList<User> users = new ArrayList<>();
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public boolean updateUser(String ID, User user) {
+        for(User u: users) {
+            if (ID.equals(u.getID())) {
+                users.set(users.indexOf(u), user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteUser(String ID) {
+        for(User u: users) {
+            if (ID.equals(u.getID())) {
+                users.remove(u);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
